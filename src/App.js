8a06f6024fcfc411d07df13d4e012e8a32b77d6a
Bhupendra Singh Parihar <bhupendraparihar@outlook.com>
2021-11-21 08:40:52 +0530

    Created custom hook useFetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import useFetch from './hooks/UseFetch';
 
-function App() {
+const App = () => {
+  const { isLoading, error, apiData } = useFetch('books', () => fetch('http://myjson.dit.upm.es/api/bins/cosr').then(res => res.json()));
+
+  console.log(apiData);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <pre>
+        { JSON.stringify(apiData, null, 4) }
+        </pre>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/hooks/UseFetch.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (key, fn) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [apiData, setApiData] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setIsLoading(true);
+        const fetchData = async () => {
+            try {
+                const resp = await fn();
+                const data = await resp[key];
+                
+                setApiData(data);
+                setIsLoading(false);
+            } catch (error) {
+                setError(error);
+                setIsLoading(false);
+            }
+        }
+    
+        fetchData();
+    }, []);
+
+    return { isLoading, apiData, error };
+}
+
+
+
+
+
+export default useFetch;
